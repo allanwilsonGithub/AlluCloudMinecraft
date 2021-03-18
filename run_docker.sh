bf199849371c370176e450e6965268c726ef89a3
Allan Wilson <allan@allanwilson.net>
2021-03-18 02:20:33 +0200

    .

--- run_docker.sh
+++ run_docker.sh
@@ -10,3 +10,12 @@ docker run \
     -p 25565:25565 \
     allu-cloud-minecraft \
     /bin/bash -c "chmod 777 scripts/change_package_json_IP_to_local_IPV4.sh && scripts/change_package_json_IP_to_local_IPV4.sh && /DATA/git/AllanWilsonMinecraft/start_minecraft_cloud.sh"
+
+docker run \
+    -d \
+    -it \
+    --security-opt seccomp:unconfined \
+    --shm-size "256M" \
+    -p 25565:25565 \
+    allu-cloud-minecraft \
+    /bin/bash -c "/DATA/git/AllanWilsonMinecraft/start_minecraft_cloud.sh"
